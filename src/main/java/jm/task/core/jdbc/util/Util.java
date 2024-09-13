85a525be88f1747c1111a933f02d221c501edad6
AntonObryvalin <catalystqy@gmail.com>
2024-09-13 15:19:18 +0600

    <<В класс Util добавил Hibernate>>

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -4,12 +4,20 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+
+// Класс для управления соединениями с базой данных и Hibernate
 public class Util {
 
+    // JDBC конфигурация
     private static final String URL = "jdbc:mysql://localhost:3306/firstlib";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "83632618";
 
+    // Создание соединения с базой данных
     public static Connection createConnection() {
         Connection connection = null;
         try {
@@ -20,5 +28,43 @@ public class Util {
             e.printStackTrace();
         }
         return connection;
+    }
+
+    // ***************** Конфигурация для Hibernate *****************
+    private static SessionFactory sessionFactory;
+
+    // Инициализация SessionFactory для Hibernate
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                configuration.configure(); // Здесь можно указать путь к файлу конфигурации, если используется XML
+                configuration.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
+                configuration.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/firstlib");
+                configuration.setProperty("hibernate.connection.username", "root");
+                configuration.setProperty("hibernate.connection.password", "83632618");
+                configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+                configuration.setProperty("hibernate.hbm2ddl.auto", "update");
+                configuration.setProperty("hibernate.show_sql", "true");
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+                System.out.println("SessionFactory создан");
+            } catch (Exception e) {
+                System.out.println("Ошибка при создании SessionFactory");
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
+
+    // Закрытие SessionFactory
+    public static void shutdown() {
+        if (sessionFactory != null) {
+            sessionFactory.close();
+            System.out.println("SessionFactory закрыт");
+        }
     }
 }
